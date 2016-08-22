69609997538a2e4b94f2d967b4b86dd944e68599
Juliana Fernandes <littlejuh@live.it>
2016-08-22 13:51:29 -0300

    Fix php mysqli extension

--- public/index.php
+++ public/index.php
@@ -1,2 +1,7 @@
 <?php
-echo "Olá mundo";
+$mysqli = new mysqli("localhost", "root", "Securepass", "mysql");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+
