a1203daf06a5edcc6903beefd896e6094bf64052
Juliana Fernandes <littlejuh@live.it>
2016-08-23 10:28:56 -0300

    Change index to hello world

--- public/index.php
+++ public/index.php
@@ -1,7 +1,3 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "Securepass", "mysql");
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-echo $mysqli->host_info . "\n";
+Echo "Olá mundo!";
 
