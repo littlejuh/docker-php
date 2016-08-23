0e19bf49c1b43a29a4c8b41cc30429c754f91504
Juliana Fernandes <littlejuh@live.it>
2016-08-23 14:40:50 -0300

    Change docker-compose version

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
-Echo "Olá mundo!";
+echo "teste";
+  
+$hostname='127.0.0.1';
+$port = '3306';
+$username='root';
+$password='secret';
 
+try {
+    $dbh = new PDO("mysql:host=$hostname;port=$port;",$username,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    echo 'Conectou.';
+}catch(PDOException $e){
+    	echo $e->getMessage();
+}
